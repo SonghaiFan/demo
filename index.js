0652ac46064b779dfa3ca4550df276ac8d0779fe
SonghaiFan <songhai.fan2022@gmail.com>
2023-09-06 07:54:45 +1000

    up

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const spec2 = "vis2.json";
+const spec1 = "vis1.json";
 const spemap = "map.json";
-vegaEmbed("#vis2", spec2)
+vegaEmbed("#vis2", spec1)
   .then(function (result) {})
   .catch(console.error);
