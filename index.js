5a7cd2e8ab14d7a57721196b6d45f7a3f33e5c90
SonghaiFan <songhai.fan2022@gmail.com>
2023-09-08 10:03:53 +1000

    final

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const spec2 = "vis2.json";
 const spec1 = "vis1.json";
 const spemap = "map.json";
-vegaEmbed("#vis2", spec1)
+vegaEmbed("#vis2", spec1, { renderer: "svg", actions: false })
   .then(function (result) {})
   .catch(console.error);
